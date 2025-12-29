ae5c7bc47f879cff95dd09350a16513e64e5f33b
Aaron Vogt <nyroxsystems@gmail.com>
2025-12-30 00:46:42 +0100

    fix: Final DROP SCHEMA to apply migration 0005
    - Migration 0005 adds schema_name field
    - DB migration history needs reset to apply it
    - This is the LAST schema drop
    - Will remove after successful deployment

--- InvenTree-master/build.sh
+++ InvenTree-master/build.sh
@@ -19,6 +19,11 @@ pip install psycopg2-binary dj-database-url django-money django-tenants==3.7.0
 
 echo "==> Running migrations..."
 cd InvenTree
+# FINAL RESET: Apply migration 0005 (schema_name field)
+# This is the LAST TIME we drop schema - migration 0005 will be applied
+echo "Final database reset to apply schema_name migration..."
+psql $DATABASE_URL -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;" || true
+
 # Run migrations on database
 # Note: migrate command tries to migrate tenants after schema migrations
 # This will fail if no tenants exist yet, but that's OK - schema migrations succeeded
