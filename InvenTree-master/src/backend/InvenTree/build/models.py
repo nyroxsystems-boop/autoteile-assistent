bc330ea27ad98d4432eb72a774cbd1951081be05
Aaron Vogt <nyroxsystems@gmail.com>
2025-12-29 04:22:05 +0100

    fix(core): correct syntax error in build models

--- InvenTree-master/src/backend/InvenTree/build/models.py
+++ InvenTree-master/src/backend/InvenTree/build/models.py
@@ -1,4 +1,6 @@
-from __future__ import annotations"""Build database model definitions."""
+from __future__ import annotations
+
+"""Build database model definitions."""
 
 import decimal
 from typing import Optional
